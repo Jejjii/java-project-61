34f0a7cade9c76b0861f9435634063281f77a953
Your Name <you@example.com>
2023-06-28 13:21:30 +0700

    update code 3

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,54 +1,43 @@
 package hexlet.code.games;
+
 import hexlet.code.Engine;
 
-@SuppressWarnings("checkstyle:Indentation")
 public class Progression {
-
-    private static final String GAME_DESCRIPTION = "What number is missing in the progression?";
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 100;
-    private static final int PROGRESSION_LENGTH = 10;
-    private static final int OPERATIONS_COUNT = 3;
+    public static final String GAME_DESCRIPTION = "What number is missing in the progression?";
+    private static final int MIN_START_NUMBER = 1;
+    private static final int MAX_START_NUMBER = 50;
+    private static final int MIN_STEP = 2;
+    private static final int MAX_STEP = 10;
+    private static final int MIN_LENGTH = 5;
+    private static final int MAX_LENGTH = 10;
 
     public static void play() {
         String[][] questionsAndAnswers = new String[Engine.ROUNDS_COUNT][2];
         for (int i = 0; i < Engine.ROUNDS_COUNT; i++) {
-
-            int[] progression = generateProgression(PROGRESSION_LENGTH);
-            int hiddenIndex = Engine.getRandomNumber(1, 9);
-            String question = formatQuestion(progression, hiddenIndex);
-            String correctAnswer = String.valueOf(progression[hiddenIndex]);
-            questionsAndAnswers[i] = new String[] {
-                    question,
-                    correctAnswer
-            };
+            int startNumber = Engine.getRandomNumber(MIN_START_NUMBER, MAX_START_NUMBER);
+            int step = Engine.getRandomNumber(MIN_STEP, MAX_STEP);
+            int length = Engine.getRandomNumber(MIN_LENGTH, MAX_LENGTH);
+            String progression = generateProgression(startNumber, step, length);
+            int missingIndex = Engine.getRandomNumber(0, length - 1);
+            String correctAnswer = String.valueOf(startNumber + (step * missingIndex));
+            String question = replaceNumberWithDots(progression, missingIndex);
+            questionsAndAnswers[i] = new String[]{question, correctAnswer};
         }
         Engine.play(GAME_DESCRIPTION, questionsAndAnswers);
     }
 
-    @SuppressWarnings("checkstyle:WhitespaceAround")
-    public static int[] generateProgression(int length) {
-        int startNumber = Engine.getRandomNumber(MIN_NUMBER, MAX_NUMBER);
-        int[] progression = new int[PROGRESSION_LENGTH];
-        int step = Engine.getRandomNumber(MIN_NUMBER, MAX_NUMBER);
-        progression[0] = startNumber;
-        for (int i = 1; i < length; i++) {
-            progression[i] = progression[i - 1] + step;
+    public static String generateProgression(int startNumber, int step, int length) {
+        StringBuilder progression = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int currentNumber = startNumber + (step * i);
+            progression.append(currentNumber).append(" ");
         }
-        return progression;
-
+        return progression.toString().trim();
     }
 
-    public static String formatQuestion(int[] progression, int hiddenIndex) {
-        StringBuilder questionBuilder = new StringBuilder();
-        for (int i = 0; i < progression.length - 1; i++) {
-            if (i == hiddenIndex) {
-                questionBuilder.append(".. ");
-            } else {
-                questionBuilder.append(progression[i]).append(" ");
-            }
-        }
-        return questionBuilder.toString().trim();
+    public static String replaceNumberWithDots(String progression, int index) {
+        String[] numbers = progression.split(" ");
+        numbers[index] = "..";
+        return String.join(" ", numbers);
     }
 }
-
